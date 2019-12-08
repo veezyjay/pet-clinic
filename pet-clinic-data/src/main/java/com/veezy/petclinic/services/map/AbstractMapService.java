a707b181c70cff6aa2d2bdc5f94ab721deb2757e
Victor Bassey <viktorbasi545@gmail.com>
2019-12-08 10:19:16 +0100

    Implement map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/veezy/petclinic/services/map/AbstractMapService.java
@@ -0,0 +1,33 @@
+package com.veezy.petclinic.services.map;
+
+import com.veezy.petclinic.services.CrudService;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> implements CrudService<T, ID> {
+    protected Map<ID, T> map = new HashMap<>();
+
+    public Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    public T findById(ID id) {
+        return map.get(id);
+    }
+
+    public T save(ID id, T object) {
+        map.put(id, object);
+        return object;
+    }
+
+    public void deleteById(ID id) {
+        map.remove(id);
+    }
+
+    public void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/veezy/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,32 @@
+package com.veezy.petclinic.services.map;
+
+import com.veezy.petclinic.model.Pet;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
